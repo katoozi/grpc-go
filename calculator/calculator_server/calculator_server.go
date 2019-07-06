f17f54fcfe311bbbcd1e34630576d66b7327dfc4
mohammad katoozi <k2527806@gmail.com>
2019-07-06 20:51:06 +0430

    write the calculator service server

--- calculator/calculator_server/calculator_server.go
+++ calculator/calculator_server/calculator_server.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
 
+	"github.com/katoozi/grpc-go-course/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	result := req.FirstNumber + req.SecondNumber
+	return &calculatorpb.SumResponse{
+		Result: result,
+	}, nil
+}
+
+func (s *server) Div(ctx context.Context, req *calculatorpb.DivRequest) (*calculatorpb.DivResponse, error) {
+	result := req.FirstNumber / req.SecondNumber
+	return &calculatorpb.DivResponse{
+		Result: result,
+	}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, req *calculatorpb.MultiplyRequest) (*calculatorpb.MultiplyResponse, error) {
+	result := req.FirstNumber * req.SecondNumber
+	return &calculatorpb.MultiplyResponse{
+		Result: result,
+	}, nil
+}
+
+func (s *server) Sub(ctx context.Context, req *calculatorpb.SubRequest) (*calculatorpb.SubResponse, error) {
+	result := req.FirstNumber - req.SecondNumber
+	return &calculatorpb.SubResponse{
+		Result: result,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculateServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
