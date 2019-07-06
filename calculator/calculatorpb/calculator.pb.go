deac8ba9bc123bc6cd935c2d392d97d2c04edb2e
mohammad katoozi <k2527806@gmail.com>
2019-07-06 22:24:48 +0430

    create PrimeNumberComposition rpc server streaming for calculate rpc service

--- calculator/calculatorpb/calculator.pb.go
+++ calculator/calculatorpb/calculator.pb.go
@@ -372,6 +372,84 @@ func (m *SubResponse) GetResult() int32 {
 	return 0
 }
 
+type PrimeNumberCompositionRequest struct {
+	Number               int32    `protobuf:"varint,1,opt,name=number,proto3" json:"number,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PrimeNumberCompositionRequest) Reset()         { *m = PrimeNumberCompositionRequest{} }
+func (m *PrimeNumberCompositionRequest) String() string { return proto.CompactTextString(m) }
+func (*PrimeNumberCompositionRequest) ProtoMessage()    {}
+func (*PrimeNumberCompositionRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_7f42938f8c8365cf, []int{8}
+}
+
+func (m *PrimeNumberCompositionRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PrimeNumberCompositionRequest.Unmarshal(m, b)
+}
+func (m *PrimeNumberCompositionRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PrimeNumberCompositionRequest.Marshal(b, m, deterministic)
+}
+func (m *PrimeNumberCompositionRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PrimeNumberCompositionRequest.Merge(m, src)
+}
+func (m *PrimeNumberCompositionRequest) XXX_Size() int {
+	return xxx_messageInfo_PrimeNumberCompositionRequest.Size(m)
+}
+func (m *PrimeNumberCompositionRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_PrimeNumberCompositionRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PrimeNumberCompositionRequest proto.InternalMessageInfo
+
+func (m *PrimeNumberCompositionRequest) GetNumber() int32 {
+	if m != nil {
+		return m.Number
+	}
+	return 0
+}
+
+type PrimeNumberCompositionResponse struct {
+	Result               int32    `protobuf:"varint,1,opt,name=result,proto3" json:"result,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PrimeNumberCompositionResponse) Reset()         { *m = PrimeNumberCompositionResponse{} }
+func (m *PrimeNumberCompositionResponse) String() string { return proto.CompactTextString(m) }
+func (*PrimeNumberCompositionResponse) ProtoMessage()    {}
+func (*PrimeNumberCompositionResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_7f42938f8c8365cf, []int{9}
+}
+
+func (m *PrimeNumberCompositionResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PrimeNumberCompositionResponse.Unmarshal(m, b)
+}
+func (m *PrimeNumberCompositionResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PrimeNumberCompositionResponse.Marshal(b, m, deterministic)
+}
+func (m *PrimeNumberCompositionResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PrimeNumberCompositionResponse.Merge(m, src)
+}
+func (m *PrimeNumberCompositionResponse) XXX_Size() int {
+	return xxx_messageInfo_PrimeNumberCompositionResponse.Size(m)
+}
+func (m *PrimeNumberCompositionResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_PrimeNumberCompositionResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PrimeNumberCompositionResponse proto.InternalMessageInfo
+
+func (m *PrimeNumberCompositionResponse) GetResult() int32 {
+	if m != nil {
+		return m.Result
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*SumRequest)(nil), "calculator.SumRequest")
 	proto.RegisterType((*SumResponse)(nil), "calculator.SumResponse")
@@ -381,6 +459,8 @@ func init() {
 	proto.RegisterType((*MultiplyResponse)(nil), "calculator.MultiplyResponse")
 	proto.RegisterType((*SubRequest)(nil), "calculator.SubRequest")
 	proto.RegisterType((*SubResponse)(nil), "calculator.SubResponse")
+	proto.RegisterType((*PrimeNumberCompositionRequest)(nil), "calculator.PrimeNumberCompositionRequest")
+	proto.RegisterType((*PrimeNumberCompositionResponse)(nil), "calculator.PrimeNumberCompositionResponse")
 }
 
 func init() {
@@ -388,25 +468,28 @@ func init() {
 }
 
 var fileDescriptor_7f42938f8c8365cf = []byte{
-	// 284 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x48, 0x4e, 0xcc, 0x49,
-	0x2e, 0xcd, 0x49, 0x2c, 0xc9, 0x2f, 0xd2, 0x47, 0x30, 0x0b, 0x92, 0x90, 0x38, 0x7a, 0x05, 0x45,
-	0xf9, 0x25, 0xf9, 0x42, 0x5c, 0x08, 0x11, 0xa5, 0x10, 0x2e, 0xae, 0xe0, 0xd2, 0xdc, 0xa0, 0xd4,
-	0xc2, 0xd2, 0xd4, 0xe2, 0x12, 0x21, 0x45, 0x2e, 0x9e, 0xb4, 0xcc, 0xa2, 0xe2, 0x92, 0xf8, 0xbc,
-	0xd2, 0xdc, 0xa4, 0xd4, 0x22, 0x09, 0x46, 0x05, 0x46, 0x0d, 0xd6, 0x20, 0x6e, 0xb0, 0x98, 0x1f,
-	0x58, 0x48, 0x48, 0x99, 0x8b, 0xb7, 0x38, 0x35, 0x39, 0x3f, 0x2f, 0x05, 0xa6, 0x86, 0x09, 0xac,
-	0x86, 0x07, 0x22, 0x08, 0x51, 0xa4, 0xa4, 0xca, 0xc5, 0x0d, 0x36, 0xb5, 0xb8, 0x20, 0x3f, 0xaf,
-	0x38, 0x55, 0x48, 0x8c, 0x8b, 0xad, 0x28, 0xb5, 0xb8, 0x34, 0xa7, 0x04, 0x6a, 0x20, 0x94, 0x07,
-	0xb2, 0xdc, 0x25, 0xb3, 0x8c, 0x06, 0x96, 0x83, 0x4d, 0x25, 0x60, 0x79, 0x24, 0x17, 0xbf, 0x6f,
-	0x69, 0x4e, 0x49, 0x66, 0x41, 0x4e, 0x25, 0xb5, 0x5d, 0xa0, 0xc5, 0x25, 0x80, 0x30, 0x9a, 0x70,
-	0x18, 0x04, 0x97, 0x26, 0xd1, 0x24, 0x02, 0x92, 0x08, 0x59, 0x6e, 0xd4, 0xc9, 0xc4, 0x25, 0xe0,
-	0x0c, 0x4d, 0x0c, 0xa9, 0xc1, 0xa9, 0x45, 0x65, 0x99, 0xc9, 0xa9, 0x42, 0x16, 0x5c, 0xcc, 0xc1,
-	0xa5, 0xb9, 0x42, 0x62, 0x7a, 0x48, 0x09, 0x07, 0x91, 0x46, 0xa4, 0xc4, 0x31, 0xc4, 0x21, 0x96,
-	0x28, 0x31, 0x80, 0x74, 0xba, 0x64, 0x96, 0xa1, 0xea, 0x44, 0x44, 0x30, 0xaa, 0x4e, 0xa4, 0x28,
-	0x52, 0x62, 0x10, 0x72, 0xe7, 0xe2, 0x80, 0x85, 0x98, 0x90, 0x34, 0xb2, 0x32, 0xb4, 0x28, 0x92,
-	0x92, 0xc1, 0x2e, 0x89, 0xec, 0x84, 0xe0, 0xd2, 0x24, 0x74, 0xc7, 0x27, 0xe1, 0x70, 0x7c, 0x12,
-	0x42, 0xa7, 0x13, 0x5f, 0x14, 0x0f, 0x72, 0xb6, 0x49, 0x62, 0x03, 0x67, 0x16, 0x63, 0x40, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0xed, 0x8a, 0xa3, 0xe0, 0x58, 0x03, 0x00, 0x00,
+	// 336 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x54, 0x4d, 0x4b, 0xc3, 0x40,
+	0x14, 0x6c, 0x2a, 0x06, 0x79, 0x8d, 0x5a, 0xf6, 0x10, 0x25, 0x7e, 0xa0, 0x11, 0xa1, 0xf6, 0x50,
+	0x45, 0x0f, 0xf6, 0x6c, 0x0b, 0x9e, 0x14, 0x69, 0xbc, 0xe8, 0x45, 0xba, 0x71, 0x85, 0x85, 0x24,
+	0x1b, 0xf7, 0x23, 0xe0, 0x9f, 0x17, 0xe9, 0x26, 0x21, 0x9b, 0xda, 0x36, 0x1e, 0xec, 0x2d, 0x3b,
+	0x99, 0x79, 0x33, 0xec, 0x1b, 0x16, 0x7a, 0xe1, 0x34, 0x0a, 0x55, 0x34, 0x95, 0x8c, 0x5f, 0x56,
+	0x9f, 0x29, 0x36, 0x0e, 0x83, 0x94, 0x33, 0xc9, 0x10, 0x54, 0x88, 0xff, 0x0c, 0x10, 0xa8, 0x78,
+	0x42, 0x3e, 0x15, 0x11, 0x12, 0x9d, 0x82, 0xf3, 0x41, 0xb9, 0x90, 0x6f, 0x89, 0x8a, 0x31, 0xe1,
+	0xfb, 0xd6, 0x89, 0xd5, 0xdb, 0x9c, 0x74, 0x34, 0xf6, 0xa8, 0x21, 0x74, 0x06, 0xdb, 0x82, 0x84,
+	0x2c, 0x79, 0x2f, 0x39, 0x6d, 0xcd, 0x71, 0x72, 0x30, 0x27, 0xf9, 0xe7, 0xd0, 0xd1, 0x53, 0x45,
+	0xca, 0x12, 0x41, 0x90, 0x0b, 0x36, 0x27, 0x42, 0x45, 0xb2, 0x18, 0x58, 0x9c, 0x66, 0xe6, 0x63,
+	0x9a, 0xad, 0xc1, 0x5c, 0x4f, 0x6d, 0x30, 0x7f, 0x81, 0xdd, 0x07, 0x15, 0x49, 0x9a, 0x46, 0x5f,
+	0xff, 0x9d, 0xa0, 0x0f, 0xdd, 0x6a, 0x74, 0xf3, 0x1d, 0x04, 0x0a, 0xaf, 0x65, 0x01, 0xb8, 0xd1,
+	0xfc, 0x16, 0x8e, 0x9e, 0x38, 0x8d, 0x49, 0xae, 0x1a, 0xb1, 0x38, 0x65, 0x82, 0x4a, 0xca, 0x92,
+	0x32, 0x8f, 0x0b, 0x76, 0x2d, 0x49, 0x71, 0xf2, 0x87, 0x70, 0xbc, 0x4c, 0xb8, 0xda, 0xf2, 0xfa,
+	0xbb, 0x0d, 0xdd, 0x51, 0xd1, 0x3f, 0x12, 0x10, 0x9e, 0xd1, 0x90, 0xa0, 0x21, 0x6c, 0x04, 0x2a,
+	0x46, 0xee, 0xc0, 0xe8, 0x6a, 0x55, 0x4b, 0x6f, 0xef, 0x17, 0x9e, 0x9b, 0xf8, 0xad, 0x99, 0x72,
+	0x4c, 0xb3, 0xba, 0xb2, 0xea, 0x54, 0x5d, 0x69, 0xb4, 0xc2, 0x6f, 0xa1, 0x7b, 0xd8, 0x2a, 0x97,
+	0x84, 0x0e, 0x4c, 0xda, 0x5c, 0x2b, 0xbc, 0xc3, 0xc5, 0x3f, 0xcd, 0x08, 0x81, 0xc2, 0xf3, 0xe1,
+	0xf1, 0x92, 0xf0, 0xd8, 0x50, 0x0a, 0x70, 0x17, 0xdf, 0x22, 0xba, 0x30, 0x45, 0x2b, 0x57, 0xe4,
+	0xf5, 0xff, 0x42, 0x2d, 0x2d, 0xaf, 0xac, 0xbb, 0x9d, 0x57, 0xc7, 0x7c, 0x1e, 0xb0, 0xad, 0x1f,
+	0x85, 0x9b, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x7e, 0x6d, 0x06, 0x21, 0x40, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -421,10 +504,13 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type CalculateServiceClient interface {
+	// unary rpc services
 	Sum(ctx context.Context, in *SumRequest, opts ...grpc.CallOption) (*SumResponse, error)
 	Div(ctx context.Context, in *DivRequest, opts ...grpc.CallOption) (*DivResponse, error)
 	Multiply(ctx context.Context, in *MultiplyRequest, opts ...grpc.CallOption) (*MultiplyResponse, error)
 	Sub(ctx context.Context, in *SubRequest, opts ...grpc.CallOption) (*SubResponse, error)
+	// server streaming rpc services
+	PrimeNumberComposition(ctx context.Context, in *PrimeNumberCompositionRequest, opts ...grpc.CallOption) (CalculateService_PrimeNumberCompositionClient, error)
 }
 
 type calculateServiceClient struct {
@@ -471,12 +557,47 @@ func (c *calculateServiceClient) Sub(ctx context.Context, in *SubRequest, opts .
 	return out, nil
 }
 
+func (c *calculateServiceClient) PrimeNumberComposition(ctx context.Context, in *PrimeNumberCompositionRequest, opts ...grpc.CallOption) (CalculateService_PrimeNumberCompositionClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_CalculateService_serviceDesc.Streams[0], "/calculator.CalculateService/PrimeNumberComposition", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &calculateServicePrimeNumberCompositionClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type CalculateService_PrimeNumberCompositionClient interface {
+	Recv() (*PrimeNumberCompositionResponse, error)
+	grpc.ClientStream
+}
+
+type calculateServicePrimeNumberCompositionClient struct {
+	grpc.ClientStream
+}
+
+func (x *calculateServicePrimeNumberCompositionClient) Recv() (*PrimeNumberCompositionResponse, error) {
+	m := new(PrimeNumberCompositionResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // CalculateServiceServer is the server API for CalculateService service.
 type CalculateServiceServer interface {
+	// unary rpc services
 	Sum(context.Context, *SumRequest) (*SumResponse, error)
 	Div(context.Context, *DivRequest) (*DivResponse, error)
 	Multiply(context.Context, *MultiplyRequest) (*MultiplyResponse, error)
 	Sub(context.Context, *SubRequest) (*SubResponse, error)
+	// server streaming rpc services
+	PrimeNumberComposition(*PrimeNumberCompositionRequest, CalculateService_PrimeNumberCompositionServer) error
 }
 
 // UnimplementedCalculateServiceServer can be embedded to have forward compatible implementations.
@@ -494,6 +615,9 @@ func (*UnimplementedCalculateServiceServer) Multiply(ctx context.Context, req *M
 }
 func (*UnimplementedCalculateServiceServer) Sub(ctx context.Context, req *SubRequest) (*SubResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Sub not implemented")
+}
+func (*UnimplementedCalculateServiceServer) PrimeNumberComposition(req *PrimeNumberCompositionRequest, srv CalculateService_PrimeNumberCompositionServer) error {
+	return status.Errorf(codes.Unimplemented, "method PrimeNumberComposition not implemented")
 }
 
 func RegisterCalculateServiceServer(s *grpc.Server, srv CalculateServiceServer) {
@@ -572,6 +696,27 @@ func _CalculateService_Sub_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CalculateService_PrimeNumberComposition_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(PrimeNumberCompositionRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(CalculateServiceServer).PrimeNumberComposition(m, &calculateServicePrimeNumberCompositionServer{stream})
+}
+
+type CalculateService_PrimeNumberCompositionServer interface {
+	Send(*PrimeNumberCompositionResponse) error
+	grpc.ServerStream
+}
+
+type calculateServicePrimeNumberCompositionServer struct {
+	grpc.ServerStream
+}
+
+func (x *calculateServicePrimeNumberCompositionServer) Send(m *PrimeNumberCompositionResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 var _CalculateService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "calculator.CalculateService",
 	HandlerType: (*CalculateServiceServer)(nil),
@@ -593,6 +738,12 @@ var _CalculateService_serviceDesc = grpc.ServiceDesc{
 			Handler:    _CalculateService_Sub_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "PrimeNumberComposition",
+			Handler:       _CalculateService_PrimeNumberComposition_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "calculator/calculatorpb/calculator.proto",
 }
