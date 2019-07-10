4c849f75f3484f2937a00de40287a50cf1a583f1
mohammad katoozi <k2527806@gmail.com>
2019-07-10 23:32:54 +0430

    write main of blog api server. include mongodb connection and grpc server

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/katoozi/grpc-go-course/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+)
+
+var collection *mongo.Collection
+
+type server struct{}
+
+type bLogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	fmt.Println("Start Blog Service. Listning...")
+
+	// make mongodb configurations. connect to mongodb
+	fmt.Println("Start Mongodb client connection...")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://@localhost:27017"))
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	ctx, cancel := context.WithTimeout(context.TODO(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	collection = client.Database("mydb").Collection("blog")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	opts := []grpc.ServerOption{}
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Start Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// wait for control + c to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// block until control + c
+	<-ch
+	fmt.Println("Stopping The Server")
+	// stopping the grpc server
+	s.Stop()
+	fmt.Println("Closing the Listener")
+	// closing the net package listner
+	lis.Close()
+	fmt.Println("Close Mongodb Client")
+	client.Disconnect(context.TODO())
+	fmt.Println("End of App.")
 }
