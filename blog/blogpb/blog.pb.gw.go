4ab4eeefbf6b01cb333ffabfc2ab9eecafdc121c
mohammad katoozi <k2527806@gmail.com>
2019-07-11 23:01:56 +0430

    integrate DeleteBlog unary rpc with grpc gateway for serving rest api at the same time

--- blog/blogpb/blog.pb.gw.go
+++ blog/blogpb/blog.pb.gw.go
@@ -89,6 +89,33 @@ func request_BlogService_UpdateBlog_0(ctx context.Context, marshaler runtime.Mar
 
 }
 
+func request_BlogService_DeleteBlog_0(ctx context.Context, marshaler runtime.Marshaler, client BlogServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DeleteBlogRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["blog_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "blog_id")
+	}
+
+	protoReq.BlogId, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "blog_id", err)
+	}
+
+	msg, err := client.DeleteBlog(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterBlogServiceHandlerFromEndpoint is same as RegisterBlogServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterBlogServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -187,6 +214,26 @@ func RegisterBlogServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("DELETE", pattern_BlogService_DeleteBlog_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_BlogService_DeleteBlog_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_BlogService_DeleteBlog_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -196,6 +243,8 @@ var (
 	pattern_BlogService_ReadBlog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"blog", "blog_id", "read"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_BlogService_UpdateBlog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"blog", "update"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_BlogService_DeleteBlog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"blog", "blog_id", "delete"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -204,4 +253,6 @@ var (
 	forward_BlogService_ReadBlog_0 = runtime.ForwardResponseMessage
 
 	forward_BlogService_UpdateBlog_0 = runtime.ForwardResponseMessage
+
+	forward_BlogService_DeleteBlog_0 = runtime.ForwardResponseMessage
 )
